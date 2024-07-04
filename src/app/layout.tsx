a2bfe58e9cc34920a50efc410aa60001f1cf55a2
Bruno Vitte <bruno_vitte@hotmail.com>
2024-07-03 19:33:22 -0600

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Cleani",
+  description: "La startup mexicana, innovando la industria de la limpieza",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
