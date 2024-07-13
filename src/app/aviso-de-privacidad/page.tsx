884f5457bf08df2813ddd7f42f969f8a34334b53
Bruno Vitte <bruno_vitte@hotmail.com>
2024-07-12 18:44:43 -0600

    add privacy policy

---
+++ src/app/aviso-de-privacidad/page.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+export default function AvisoDePrivacidad() {
+  return (
+    <main className='flex justify-center'>
+      <div className="container px-10">
+        <h1 className='text-4xl font-bold my-6'>Política de privacidad</h1>
+        <p className='text-justify py-6'>
+          En este aviso de privacidad se regula la forma en la que los datos recabados por medio de la aplicación (&quot;<span className='font-bold'>Cleani</span>&quot;), propiedad de <span className='font-bold'>WEHOUSEKEEP MEXICO</span>, o cualquier otro sitio propiedad de la misma empresa. Los datos en cuestión serán tratados conforme lo presente.
+        </p>
+
+        <ol className='list-decimal px-6'>
+          <li>
+            Sobre los datos recabados
+            <p className='text-justify py-6'>
+              <span className='font-bold'>WEHOUSEKEEP MEXICO</span> no venderá, intercambiará, compartirá ni difundirá de ninguna manera a terceros ajenos los datos personales recabados a través de Cleani, sin autorización del titular de los datos. Los datos personales recabados incluyen, entre otras cosas, el correo electrónico del usuario, su nombre de usuario, las claves de acceso de las cuentas que enlace a su cuenta de Cleani, su nombre completo, su domicilio, su ubicación geográfica, su dirección IP, las imágenes que el usuario suba de su documento de identidad, su(s) número(s) de teléfono y/o aquellos datos previstos como personales por la ley correspondiente.
+            </p>
+          </li>
+          <li>
+            Sobre la autorización
+            <p className="text-justify py-6">
+              Al crear una cuenta en <span className='font-bold'>Cleani</span>, autorizas que estos datos serán usados por <span className='font-bold'>WEHOUSEKEEP MEXICO</span> para la creación de perfiles de datos, los cuales tendrán usos estadísticos para <span className='font-bold'>WEHOUSEKEEP MEXICO.</span> Asimismo, en caso de que el presente aviso sea modificado, la eliminación de la cuenta será entendida como la manera en la que se revoca la autorización del aviso.
+            </p>
+          </li>
+          <li>
+            Sobre los derechos ARCO
+            <p className="text-justify py-6">
+              En cualquier momento el usuario tendrá derecho a solicitar el acceso, rectificación, cancelación u oposición de sus datos personales. Estos derechos incluyen la opción de que el usuario pueda pedir que sus datos personales sean removidos en su totalidad de las bases de datos, en las cuales <span className='font-bold'>WEHOUSEKEEP MEXICO</span> hará el tratamiento de los datos. En este supuesto, <span className='font-bold'>WEHOUSEKEEP MEXICO</span> tomará las acciones que considere pertinentes, las cuales incluyen que para la solicitud de este proceso se deba pedir la eliminación de la cuenta desde Cleani, para que <span className='font-bold'>WEHOUSEKEEP MEXICO</span> contacte al usuario que desee borrar sus datos. Posteriormente, <span className='font-bold'>WEHOUSEKEEP MEXICO</span> borrará los datos, una vez que la solicitud sea completada.
+            </p>
+          </li>
+          <li>
+            Sobre los fines del tratamiento de datos
+            <p className="text-justify py-6">
+              <span className='font-bold'>WEHOUSEKEEP MEXICO</span> usará los datos personales recabados para:
+            </p>
+            <ol className="list-roman px-8 ">
+              <li>
+                Para fines administrativos, en los que sea conveniente usar los datos personales para brindar los servicios y productos que <span className='font-bold'>WEHOUSEKEEP MEXICO</span> ofrece a sus usuarios.
+              </li>
+              <li>
+                Notificar al usuario los cambios que hayan tanto en el aviso presente, como los Terminos y Condiciones, o cualquier otro cambio relevante al usuario.
+              </li>
+              <li>
+                Comunicarle al usuario las distintas promociones o eventos relacionados a <span className='font-bold'>Cleani</span> que <span className='font-bold'>WEHOUSEKEEP MEXICO</span> considere relevantes para sus usuarios.
+              </li>
+              <li>
+                Contactar al usuario por cuestiones que surjan del uso de <span className='font-bold'>Cleani</span> o de sus servicios, o de cualquier otra circunstancia relacionada a los productos o servicios que <span className='font-bold'>WEHOUSEKEEP MEXICO</span> ofrece a sus clientes.
+              </li>
+              <li>
+                La creación de perfiles estadísticos, en <span className='font-bold'>Cleani</span> o cualquier otro sitio propiedad de <span className='font-bold'>WEHOUSEKEEP MEXICO</span>, para otorgarle al usuario una experiencia personalizada.
+              </li>
+            </ol>
+            <p className="text-justify py-6">
+              Los fines expuestos son necesarios para que <span className='font-bold'>WEHOUSEKEEP MEXICO</span> pueda ofrecerle la mejor experiencia posible a los usuario a través de <span className='font-bold'>Cleani</span> y los demás sitios que sean propiedad de <span className='font-bold'>WEHOUSEKEEP MEXICO</span>.
+            </p>
+          </li>
+          <li>
+            Activación de cuenta
+            <p className="text-justify pt-6 pb-3">
+              Por motivos de seguridad y para garantizar la protección de tus datos personales, el acto de crear una cuenta en nuestra aplicación implica el consentimiento explícito del usuario al tratamiento de sus datos, de acuerdo con este aviso de privacidad. Este proceso incluye la verificación de tu número telefónico y la carga de un documento que acredite suidentidad, asegurando así la autenticidad y seguridad de la información proporcionada.
+            </p>
+            <p className="text-justify pt-3 pb-6">
+              Por las mismas cuestiones de seguridad, al darte de alta en nuestro sitio y consentir el tratamiento de tus datos conforme al presente aviso de privacidad, te será enviado un correo de verificación y validación que deberás de confirmar para concluir tu aceptación y registro.
+            </p>
+          </li>
+          <li>
+            Garantía de seguridad
+            <p className="text-justify pt-6 pb-3">
+              No obstante que <span className='font-bold'>WEHOUSEKEEP MEXICO</span> utiliza la tecnología más avanzada para salvaguardar la información personal de nuestros usuarios; sin embargo, <span className='font-bold'>WEHOUSEKEEP MEXICO</span> no se hace responsable por problemas de seguridad que deriven de fuerza mayor o caso fortuito. En este sentido, <span className='font-bold'>WEHOUSEKEEP MEXICO</span> se compromete a implementar y mantener medidas de seguridad físicas, administrativas y técnicas adecuadas para proteger los datos personales de nuestras empleadas y clientes. Estas medidas incluyen, pero no se limitan a, encriptación de datos, controles de acceso restringido y protocolos de seguridad para la comunicación de datos.
+            </p>
+            <p className="text-justify py-3">
+              Además, <span className='font-bold'>WEHOUSEKEEP MEXICO</span> actualiza sus protocolos conforme a las últimas tendencias y recomendaciones en seguridad de la información. En caso de que se detecte alguna brecha de seguridad, <span className='font-bold'>WEHOUSEKEEP MEXICO</span> se compromete a actuar de manera rápida y responsable para mitigar los riesgos y notificar a los afectados conforme a lo establecido en la legislación aplicable.
+            </p>
+            <p className="text-justify pt-3 pb-6">
+              Para mayor protección, también animamos a nuestras empleadas y clientes a tomar medidas de precaución personal, como mantener la confidencialidad de sus contraseñas y otros datos de acceso, y estar en alerta ante cualquier actividad sospechosa en sus cuentas.
+            </p>
+          </li>
+        </ol>
+
+
+
+
+      </div>
+    </main >
+  )
+}
