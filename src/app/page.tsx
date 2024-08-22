651b6a0d9ef019432109ad649a20171fd379c897
Bruno Vitte <bruno_vitte@hotmail.com>
2024-08-22 11:21:53 -0600

    t3mp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,27 @@
-"use client"
-import { useEffect } from "react";
-
+'use client';
 import LandigPageScreen from "@/components/screens/landing-page/landing-page.screen";
 import MainPage from "@/components/screens/main-page/main-page.screen";
-import jQuery from "jquery";
 import M from "materialize-css";
+import { useEffect } from "react";
 
 export default function Home() {
-  let navBarHidden: boolean = false
-  const initPushpin = async () => {
-    M.Pushpin.init(jQuery('.pushpin'));
-    jQuery('.pushpin').each(function (element) {
-      var _this = jQuery(this);
-      var _target = jQuery('#' + jQuery(this).attr('data-target'));
-
-      console.log(_target?.offset());
-
-      M.Pushpin.init(_this, {
-        top: _target?.offset()?.top || 0,
-        bottom: (_target?.offset()?.top || 0) + (_target?.outerHeight() || 0) - (_this.height() || 0)
-      });
-    });
-  }
   useEffect((): void => {
-    initPushpin();
-  }, [])
+    if (typeof window === "object") {
+      const jQuery = require('jquery')
+      const initPushpin = async () => {
+        M.Pushpin.init(jQuery('.pushpin'));
+        jQuery('.pushpin').each(() => {
+          var _this = jQuery(this);
+          var _target = jQuery('#' + jQuery(this).attr('data-target'));
+          M.Pushpin.init(_this, {
+            top: _target?.offset()?.top || 0,
+            bottom: (_target?.offset()?.top || 0) + (_target?.outerHeight() || 0) - (_this.height() || 0)
+          });
+        });
+      }
+      initPushpin();
+    }
+  })
 
   return (
     <>
